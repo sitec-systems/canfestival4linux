ecdc50bdf666b1e19c381d4694120a286b07a3eb
Robert Lehmann <robert.lehmann@sitec-systems.de>
2026-01-09 14:43:01 +0100

    can: Reformat file

--- include/can.h
+++ include/can.h
@@ -1,12 +1,11 @@
 /*
-This file is part of CanFestival, a library implementing CanOpen Stack. 
+This file is part of CanFestival, a library implementing CanOpen Stack.
 
 Copyright (C): Edouard TISSERANT and Francis DUPIN
 
 See COPYING file for copyrights details.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public
 License as published by the Free Software Foundation; either
 version 2.1 of the License, or (at your option) any later version.
 
@@ -25,18 +24,19 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "applicfg.h"
 
-/** 
- * @brief The CAN message structure 
+/**
+ * @brief The CAN message structure
  * @ingroup can
  */
 typedef struct {
-  UNS16 cob_id;	/**< message's ID */
-  UNS8 rtr;		/**< remote transmission request. (0 if not rtr message, 1 if rtr message) */
-  UNS8 len;		/**< message's length (0 to 8) */
-  UNS8 data[8]; /**< message's datas */
+    UNS16 cob_id; /**< message's ID */
+    UNS8 rtr; /**< remote transmission request. (0 if not rtr message, 1 if rtr
+                 message) */
+    UNS8 len; /**< message's length (0 to 8) */
+    UNS8 data[8]; /**< message's datas */
 } Message;
 
-#define Message_Initializer {0,0,0,{0,0,0,0,0,0,0,0}}
+#define Message_Initializer {0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0}}
 
 typedef UNS8 (*canSend_t)(Message *);
 
